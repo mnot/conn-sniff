6595dd5f9ae86454fe4046c2d4f02d66157824d2
Mark Nottingham <mnot@mnot.net>
2018-04-20 17:12:34 +1000

    show process, do dns lookup

--- conn-sniff.js
+++ conn-sniff.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-var sprintf = require('sprintf-js').sprintf
-var Cap = require('cap').Cap
-var decoders = require('cap').decoders
-var PROTOCOL = decoders.PROTOCOL
+const dns = require('dns')
+const lsof = require('lsof')
+const sprintf = require('sprintf-js').sprintf
+const Cap = require('cap').Cap
+const decoders = require('cap').decoders
+const PROTOCOL = decoders.PROTOCOL
 
 var c = new Cap()
 var device = 'en0'
@@ -15,24 +17,53 @@ var linkType = c.open(device, filter, bufSize, buffer)
 
 c.setMinBytes && c.setMinBytes(0)
 
+var defaultHandler = {
+  get: function (target, name) {
+    return target.hasOwnProperty(name) ? target[name] : name
+  }
+}
+
 var openConns = {}
+var openPorts = {}
 var ttlConns = {}
 var byteCounts = {}
+var hostNames = new Proxy({}, defaultHandler)
 
 function log (type, dstaddr, connId) {
-  data = {
-    'type': type,
-    'dst': dstaddr,
-    'open': openConns[dstaddr].size,
-    'ttl': ttlConns[dstaddr]
-  }
-  var output = sprintf("%(type)5s %(dst)15s open: %(open)3i ttl: %(ttl)3i", data)
-  if (connId) {
-    data['in'] = byteCounts[connId][0]
-    data['out'] = byteCounts[connId][1] 
-    output += sprintf(" out bytes: %(out)7i in bytes: %(in)7i", data)
-  }
-  console.log(output)    
+  setTimeout(function () {
+    var data = {
+      'type': type,
+      'dst': hostNames[dstaddr],
+      'open': openConns[dstaddr].size,
+      'ttl': ttlConns[dstaddr]
+    }
+    var output = sprintf('%(type)5s %(dst)60s open: %(open)3i ttl: %(ttl)3i', data)
+    if (connId) {
+      data['in'] = byteCounts[connId][0]
+      data['out'] = byteCounts[connId][1]
+      output += sprintf(' out bytes: %(out)7i in bytes: %(in)7i', data)
+    }
+    console.log(output)    
+  }, 100)
+}
+
+function getProcess (srcPort) {
+  lsof.rawTcpPort(srcPort, function (data) {
+    if (data.length > 0) {
+      openPorts[srcPort] = data[0]['command']
+    }
+  })
+}
+
+function getDns (ip) {
+  dns.reverse(ip, function (err, hostnames) {
+    if (err) {
+      return
+    }
+    if (hostnames.length > 0) {
+      hostNames[ip] = hostnames[0]
+    }
+  })
 }
 
 c.on('packet', function (nbytes, trunc) {
@@ -50,19 +81,22 @@ c.on('packet', function (nbytes, trunc) {
         var datalen = ret.info.totallen - ret.hdrlen
         ret = decoders.TCP(buffer, ret.offset)
         datalen -= ret.hdrlen
-        if (ret.info.dstport === 443) {  // outbound
+        var connId
+        if (ret.info.dstport === 443) { // outbound
           var srcport = ret.info.srcport
-          var connId = srcport + ' ' + dstaddr
+          connId = srcport + ' ' + dstaddr
           if (!byteCounts[connId]) {
-            byteCounts[connId] = [0,0]
+            byteCounts[connId] = [0, 0]
           }
           byteCounts[connId][1] += datalen
-          
+
           var isFin = ret.info.flags & 0x1
           var isSyn = ret.info.flags & 0x2
           var isRst = ret.info.flags & 0x4
           if (isSyn) {
             openConns[dstaddr].add(srcport)
+            getProcess(srcport)
+            getDns(dstaddr)
             ttlConns[dstaddr] += 1
             log('START', dstaddr)
           }
@@ -72,23 +106,39 @@ c.on('packet', function (nbytes, trunc) {
           }
           if (isFin) {
             openConns[dstaddr].delete(srcport)
+            delete openPorts[dstaddr]
             log('FIN', dstaddr, connId)
-            byteCounts[connId] = [0,0]
+            byteCounts[connId] = [0, 0]
           }
           if (isRst) {
             openConns[dstaddr].delete(srcport)
+            delete openPorts[dstaddr]
             log('FIN', dstaddr, connId)
-            byteCounts[connId] = [0,0]
-          }                    
+            byteCounts[connId] = [0, 0]
+          }
         } else { // inbound
-          var connId = ret.info.dstport + ' ' + srcaddr
+          connId = ret.info.dstport + ' ' + srcaddr
           if (!byteCounts[connId]) {
-            byteCounts[connId] = [0,0]
+            byteCounts[connId] = [0, 0]
           }
           byteCounts[connId][0] += datalen
-
         }
       }
     }
   }
 })
+
+setInterval(function () {
+  for (var conn in openConns) {
+    var connPorts = openConns[conn].values()
+    var data = {
+      'dst': hostNames[conn],
+      'count': openConns[conn].size,
+      'name': openPorts[connPorts.next().value]
+    }
+    if (data['count'] === 0) {
+      continue
+    }
+    console.log(sprintf('      %(dst)60s %(name)20s open: %(count)3i', data))
+  }
+}, 10 * 1000)
